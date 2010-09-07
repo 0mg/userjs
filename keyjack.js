348e702c69003b1706a1e9b12628026139fba0be
0mg <ray.0mg.ppppp@gmail.com>
2010-09-08 07:22:39 +0900

    updated

--- keyjack.js
+++ keyjack.js
@@ -4,8 +4,14 @@
 // @include http://*.google.com/*
 // ==/UserScript==
 
-addEventListener("keypress", function(v) {
-  if (location.host.slice(-10) === "google.com" && v.target.name === "q" &&
-  (v.keyCode === 38 || v.keyCode === 40) && !v.shiftKey) return;
-  v.stopPropagation();
-}, true);
+addEventListener("keypress",
+  location.host.slice(-10) === "google.com" ?
+  function(v) {
+    if (v.target.name === "q" && (v.keyCode === 38 || v.keyCode === 40) &&
+    !v.shiftKey) return;
+    v.stopPropagation();
+  } :
+  function(v) {
+    v.stopPropagation();
+  },
+true);
