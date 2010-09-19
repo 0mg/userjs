75d8a12384d35485548c8ab036893046db3bbb47
0mg <ray.0mg.ppppp@gmail.com>
2010-09-19 14:36:53 +0900

    updated

--- keyjack.js
+++ keyjack.js
@@ -3,15 +3,18 @@
 // @include http://*.hatena.ne.jp/*
 // @include http://*.google.com/*
 // @include http://*.google.co.jp/*
+// @include http://*.tumblr.com/*
 // ==/UserScript==
 
-addEventListener("keypress", (location.host.slice(-10) === "google.com" ||
-location.host.slice(-12) === "google.co.jp") ?
-function(v) {
-  if (v.target.name === "q" && (v.keyCode === 38 || v.keyCode === 40) &&
-  !v.shiftKey) return;
-  v.stopPropagation();
-} :
-function(v) {
-  v.stopPropagation();
-}, true);
+(function() {
+  function keyJack(v) {
+    if ((location.host.slice(-10) === "google.com" ||
+        location.host.slice(-12) === "google.co.jp") &&
+        v.target.name === "q" && (v.keyCode === 38 || v.keyCode === 40) &&
+        !v.shiftKey) return;
+    else v.stopPropagation();
+  };
+  addEventListener("keypress", keyJack, true);
+  addEventListener("keydown", keyJack, true);
+  addEventListener("keyup", keyJack, true);
+})();
