20682b0d71c59e05ff7759f5a621b1aa3c5b8bb6
0mg <ray.0mg.ppppp@gmail.com>
2010-09-20 18:03:49 +0900

    updated

--- keyjack.js
+++ keyjack.js
@@ -6,8 +6,8 @@
   function keyJack(v) {
     if (v.keyCode === 13) return;
     else if (/\.google\.|\.youtube\./test(location.host) &&
-        v.target.name === "q" && (v.keyCode === 38 || v.keyCode === 40) &&
-        !v.shiftKey) return;
+             v.target.name === "q" && (v.keyCode === 38 || v.keyCode === 40) &&
+             !v.shiftKey) return;
     else v.stopPropagation();
   };
   addEventListener("keypress", keyJack, true);
