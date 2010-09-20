9e52fcbfb0963fe3a0302dc4ed41672bb5716cb2
0mg <ray.0mg.ppppp@gmail.com>
2010-09-20 17:53:26 +0900

    updated

--- keyjack.js
+++ keyjack.js
@@ -1,15 +1,11 @@
 // ==UserScript==
 // @name Keyboard Hijack Hijack
-// @include http://*.hatena.ne.jp/*
-// @include http://*.google.com/*
-// @include http://*.google.co.jp/*
-// @include http://*.tumblr.com/*
 // ==/UserScript==
 
 (function() {
   function keyJack(v) {
-    if ((location.host.slice(-10) === "google.com" ||
-        location.host.slice(-12) === "google.co.jp") &&
+    if (v.keyCode === 13) return;
+    else if (/\.google\.|\.youtube\./test(location.host) &&
         v.target.name === "q" && (v.keyCode === 38 || v.keyCode === 40) &&
         !v.shiftKey) return;
     else v.stopPropagation();
