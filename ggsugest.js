5ae866966a68d47860aa5d7989835e50f9e8af19
0mg <ray.0mg.ppppp@gmail.com>
2010-09-20 21:14:52 +0900

    updated

--- ggsugest.js
+++ ggsugest.js
@@ -3,6 +3,8 @@
 // ==/UserScript==
 
 addEventListener("keypress", function(v) {
-  if (v.target.autocomplete && (v.keyCode === 38 || v.keyCode === 40) &&
-      !v.shiftKey) v.preventDefault();
+  if (v.target.autocomplete && (v.keyCode === 38 || v.keyCode === 40)) {
+    if (v.shiftKey) v.stopPropagation();
+    else v.preventDefault();
+  }
 }, true);
--- keyjack.js
+++ keyjack.js
@@ -1,15 +1,12 @@
 // ==UserScript==
 // @name Keyboard Hijack Hijack
-// @exclude http://www.youtube.com/*
 // ==/UserScript==
 
 (function() {
   function keyJack(v) {
-    if (v.keyCode === 13) return;
-    else if (~location.host.indexOf(".google.") &&
-             v.target.autocomplete && (v.keyCode === 38 || v.keyCode === 40) &&
-             !v.shiftKey) return;
-    else v.stopPropagation();
+    if (v.target instanceof HTMLInputElement ||
+    v.target instanceof HTMLTextAreaElement) return;
+    v.stopPropagation();
   };
   addEventListener("keypress", keyJack, true);
   addEventListener("keydown", keyJack, true);
