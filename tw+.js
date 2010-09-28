ede727347687587a932aa47051e85ef30f567569
0mg <ray.0mg.ppppp@gmail.com>
2010-09-29 06:52:41 +0900

    updated

--- tw+.js
+++ tw+.js
@@ -36,11 +36,7 @@ addEventListener("DOMContentLoaded", function() {
     xhr.send(null);
   };
   function post(u, q, f) {
-    get("/about/contact", function(xhr) {
-      var data = xhr.responseText;
-      var key = '<input name="authenticity_token" value="';
-      var auth = data.substr(data.indexOf(key) + key.length, 40);
-
+    auth(function(auth) {
       xhr = new XMLHttpRequest;
       xhr.open("POST", u, true);
       xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -54,60 +50,160 @@ addEventListener("DOMContentLoaded", function() {
       xhr.send(q + "&post_authenticity_token=" + auth);
     });
   };
+  function auth(f) {
+    get("/about/contact", function(xhr) {
+      var data = xhr.responseText;
+      var key = '<input name="authenticity_token" value="';
+      var auth = data.substr(data.indexOf(key) + key.length, 40);
+      f(auth);
+    });
+  };
+  function listize() {
+    var f = document.createDocumentFragment();
+    arguments.forEach(function(o) {
+      var li = ce("li");
+      li.appendChild(o);
+      f.appendChild(li);
+    });
+    return f;
+  };
 
-  /* SCREEN INITIALIZE */
-  function initDOM() {
+
+
+
+
+  /* Main Modules */
+  function initDOM(my) {
     document.removeChild(document.documentElement);
-  
+
     var html = ce("html");
     var head = ce("head");
     var title = ce("title");
     var style = ce("style");
     var body = ce("body");
-  
+
+    html.style.height = "100%";
+
     title.appendChild(ct("tw+"));
     style.appendChild(ct("\
     "));
-  
+
     head.appendChild(title);
     head.appendChild(style);
-  
+
+    body.appendChild(makeGlobalBar(my));
+
     html.appendChild(head);
     html.appendChild(body);
-  
+
     document.appendChild(html);
-    html.style.height = "100%";
-    html.style.display = "block";
-  };
-
-  /* BUILD */
-
-  function getMyself(xhr) {
-    var my = JSON.parse(xhr.responseText)[0].user;
-    applyMyself(my);
   };
 
 
 
 
 
+  function makeGlobalBar(my) {
+    var g = {
+      bar: ce("ul"),
+      home: ce("a"),
+      profile: ce("a"),
+      replies: ce("a"),
+      following: ce("a"),
+      followers: ce("a"),
+      lists: ce("a"),
+      listed: ce("a"),
+      blocking: ce("a"),
+      logout: ce("button")
+    };
 
-  function applyMyself(my) {
-    var hash = location.pathname.slice(3).replace(/[/]+$/, "").split("/");
-    var query = location.search.slice(1);
+    g.bar.id = "globalbar";
+
+    g.home.href = "/+/";
+    g.home.appendChild(ct("Home"));
+
+    g.profile.href = "/+/" + my.screen_name;
+    g.profile.appendChild(ct("Profile"));
+
+    g.replies.href = "/+/replies";
+    g.replies.appendChild(ct("@" + my.screen_name));
+
+    g.following.href = "/+/following";
+    g.following.appendChild(ct("Following:" + my.friends_count));
+
+    g.followers.href = "/+/followers";
+    g.followers.appendChild(ct("Followers:" + my.followers_count));
+
+    g.lists.href = "/+/" + my.screen_name + "/lists";
+    g.lists.appendChild(ct("Lists"));
+
+    g.listed.href = "/+/" + my.screen_name + "/lists/memberships";
+    g.listed.appendChild(ct("Listed:" + my.listed_count));
+
+    g.blocking.href = "/+/blocking";
+    g.blocking.appendChild(ct("Blocking"));
+
+    g.logout.onclick = function() {
+      if (confirm("logout Sure?")) {
+        post("/sessions/destroy", "", function(xhr) { location = "/+/"; });
+      }
+      return false;
+    };
+    g.logout.appendChild(ct("logout"));
+
+    g.bar.appendChild(listize(
+      g.home,
+      g.profile,
+      g.replies,
+      g.following,
+      g.followers,
+      g.lists,
+      g.listed,
+      g.blocking,
+      g.logout
+    ));
+    return g.bar;
+  };
+
+
+
+
+
+  function getPage(my) {
+    var key = location.pathname.slice(3).replace(/[/]+$/, "");
+    var hash = key.split("/");
+    var q = location.search.slice(1);
     switch (hash.length) {
       case (1): {
         switch (hash[0]) {
+          case ("lists"): {
+            showLists("/1/" + my.id + "/lists.json?" + q + "&cursor=-1", my);
+            showLists("/1/" + my.id + "/lists/subscriptions.json?" + q, my);
+            break;
+          }
           case ("following"): {
+            showUsers("/1/statuses/friends.json?" + q + "&cursor=-1", my);
+            break;
+          }
+          case ("followers"): {
+            showUsers("/1/statuses/followers.json?" + q + "&cursor=-1", my);
+            break;
+          }
+          case ("replies"): {
+            showTL("/1/statuses/mentions.json?" + q, my);
+            break;
+          }
+          case ("blocking"): {
+            showUsers("/1/blocks/blocking.json?" + q, my);
             break;
           }
           case (""): {
-            get("/1/statuses/home_timeline.json?" + query, showTL);
+            showTL("/1/statuses/home_timeline.json?" + q, my);
             break;
           }
           default: {
-            get("/1/statuses/user_timeline.json?screen_name=" + hash[0] +
-            "&" + query, showTL);
+            showTL("/1/statuses/user_timeline.json?screen_name=" + hash[0] +
+            "&" + q, my);
             break;
           }
         }
@@ -115,15 +211,30 @@ addEventListener("DOMContentLoaded", function() {
       }
       case (2): {
         switch (hash[1]) {
+          case ("following"): {
+            showUsers("/1/statuses/friends.json?screen_name=" + hash[0] + "&" +
+            q + "&cursor=-1", my);
+            break;
+          }
+          case ("followers"): {
+            showUsers("/1/statuses/followers.json?screen_name=" + hash[0] +
+            "&" + q + "&cursor=-1", showUsers);
+            break;
+          }
           case ("lists"): {
-            get("/1/" + hash[0] + "/lists.json?" + query, showLists);
-            get("/1/" + hash[0] + "/lists/subscriptions.json?" + query,
-            showLists);
+            showLists("/1/" + hash[0] + "/lists.json?" + q, my);
+            showLists("/1/" + hash[0] + "/lists/subscriptions.json?" + q, my);
+            break;
+          }
+          case ("memberships"): {
+            if (hash[0] === "lists") {
+              showLists("/1/" + hash.join("/") + ".json?" + q, my);
+            }
             break;
           }
           default: {
-            get("/1/" + hash[0] + "/lists/" + hash[1] +
-            "/statuses.json?" + query, showTL);
+            showTL("/1/" + hash[0] + "/lists/" + hash[1] +
+            "/statuses.json?" + q, showTL);
             break;
           }
         }
@@ -132,8 +243,14 @@ addEventListener("DOMContentLoaded", function() {
       case (3): {
         switch (hash[2]) {
           case ("members"): {
-            get("/1/" + hash[0] + "/" + hash[1] + "/members.json?" + query,
-            showUsers);
+            showUsers("/1/" + hash[0] + "/" + hash[1] + "/members.json?" + q,
+            my);
+            break;
+          }
+          case ("memberships"): {
+            if (hash[1] === "lists") {
+              showLists("/1/" + hash.join("/") + ".json?" + q, my);
+            }
             break;
           }
         }
@@ -146,16 +263,27 @@ addEventListener("DOMContentLoaded", function() {
 
 
 
-  function makeActbar() {
-    var actbar = {};
-    actbar.form = ce("div");
-    actbar.source = ce("input");
-    actbar.target = ce("input");
-    actbar.add = ce("input");
-    actbar.rm = ce("input");
-    actbar.submit = ce("input");
+  function makeActbar(my) {
+    var actbar = {
+      form: ce("form"),
+      source: ce("input"),
+      target: ce("input"),
+      add: ce("input"),
+      rm: ce("input"),
+      submit: ce("input")
+    };
+
+    actbar.add.label = ce("label");
+    actbar.rm.label = ce("label");
+
+    actbar.add.label.appendChild(actbar.add);
+    actbar.add.label.appendChild(ct("add"));
+    actbar.rm.label.appendChild(actbar.rm);
+    actbar.rm.label.appendChild(ct("delete"));
 
     actbar.source.type = "text";
+    actbar.source.value =
+    location.pathname.slice(3).match(/[^/]+(?:[/][^/]+)?/);
     actbar.target.type = "text";
     actbar.add.type = "radio";
     actbar.add.checked = true;
@@ -164,22 +292,60 @@ addEventListener("DOMContentLoaded", function() {
     actbar.submit.value = "POST";
 
     actbar.submit.addEventListener("click", function(v) {
-      post("/1/" + actbar.source.value + "/members.json",
-      "id=" + actbar.target.value +
-      (actbar.rm.checked ? "&_method=DELETE" : ""),
-      function(xhr) {
-        alert(xhr.getAllResponseHeaders());
-      });
+      if (actbar.source.value === "following" ||
+      actbar.source.value === my.screen_name + "/following") {
+        if (actbar.add.checked) {
+          post("/1/friendships/create/" + actbar.target.value + ".json", "",
+          function(xhr) {
+            alert(xhr.status);
+          });
+        } else if (actbar.rm.checked) {
+          post("/1/friendships/destroy/" + actbar.target.value + ".json", "",
+          function(xhr) {
+            alert(xhr.status);
+          });
+        }
+      } else if (actbar.source.value === "followers" ||
+      actbar.source.value === my.screen_name + "/followers") {
+        if (actbar.add.checked) {
+          post("/1/blocks/destroy/" + actbar.target.value + ".json", "",
+          function(xhr) {
+            alert(xhr.status);
+          });
+        } else if (actbar.rm.checked) {
+          post("/1/blocks/create/" + actbar.target.value + ".json", "",
+          function(xhr) {
+            alert(xhr.status);
+          });
+        }
+      } else if (actbar.source.value === "blocking") {
+        if (actbar.add.checked) {
+          post("/1/blocks/create/" + actbar.target.value + ".json", "",
+          function(xhr) {
+            alert(xhr.status);
+          });
+        } else if (actbar.rm.checked) {
+          post("/1/blocks/destroy/" + actbar.target.value + ".json", "",
+          function(xhr) {
+            alert(xhr.status);
+          });
+        }
+      } else {
+        post("/1/" + actbar.source.value + "/members.json",
+        "id=" + actbar.target.value +
+        (actbar.rm.checked ? "&_method=DELETE" : ""),
+        function(xhr) {
+          alert(xhr.status);
+        });
+      }
     }, false);
 
     actbar.form.appendChild(ct("source: "));
     actbar.form.appendChild(actbar.source);
     actbar.form.appendChild(ct("target: "));
     actbar.form.appendChild(actbar.target);
-    actbar.form.appendChild(actbar.add);
-    actbar.form.appendChild(ct("add"));
-    actbar.form.appendChild(actbar.rm);
-    actbar.form.appendChild(ct("delete"));
+    actbar.form.appendChild(actbar.add.label);
+    actbar.form.appendChild(actbar.rm.label);
     actbar.form.appendChild(actbar.submit);
     return actbar.form;
   };
@@ -189,24 +355,26 @@ addEventListener("DOMContentLoaded", function() {
 
 
 
-  function showUsers(xhr) {
-    var data = JSON.parse(xhr.responseText);
-    var users = data.users;
+  function showUsers(u, my) {
+    get(u, function(xhr) {
+      var data = JSON.parse(xhr.responseText);
+      var users = data.users || data;
 
-    var ul = ce("ul");
-    users.forEach(function(s) {
-      var li = ce("li");
-      var a = ce("a");
-      a.href = "/+/" + s.screen_name;
-      a.appendChild(ct(s.screen_name));
-      li.appendChild(a);
-      ul.appendChild(li);
+      var ul = ce("ul");
+      users && users.forEach(function(s) {
+        var li = ce("li");
+        var a = ce("a");
+        a.href = "/+/" + s.screen_name;
+        a.appendChild(ct(s.screen_name));
+        li.appendChild(a);
+        ul.appendChild(li);
+      });
+
+      ul.appendChild(makeCursor(data));
+
+      document.body.appendChild(makeActbar(my));
+      document.body.appendChild(ul);
     });
-
-    ul.appendChild(makeCursor(data));
-
-    document.body.appendChild(makeActbar());
-    document.body.appendChild(ul);
   };
 
 
@@ -247,63 +415,84 @@ addEventListener("DOMContentLoaded", function() {
 
 
 
-  function showLists(xhr) {
-    var data = JSON.parse(xhr.responseText);
-    var lists = data.lists;
+  function showLists(u, my) {
+    get(u, function(xhr) {
+      var data = JSON.parse(xhr.responseText);
+      var lists = data.lists;
 
-    var ul = ce("ul");
+      var ul = ce("ul");
 
-    lists.forEach(function(l) {
-      var li = ce("li");
-      var a = ce("a");
+      lists.forEach(function(l) {
+        var li = ce("li");
+        var a = ce("a");
 
-      a.href = "/+/" + l.full_name.slice(1);
-      a.appendChild(ct(l.full_name));
+        a.href = "/+/" + l.full_name.slice(1);
+        a.appendChild(ct(l.full_name));
 
-      li.appendChild(a);
+        li.appendChild(a);
 
-      ul.appendChild(li);
-    });
-
-    document.body.appendChild(makeCursor(data));
-    document.body.appendChild(ul);
-  };
-
-
-
-
-
-  function showTL(xhr) {
-    var TL = JSON.parse(xhr.responseText);
-    var ul = ce("ul");
-
-    var past = ce("li");
-    past.a = ce("a");
-
-    past.a.appendChild(ct("past"));
-    past.appendChild(past.a);
-
-    TL.forEach(function(s, i) {
-      if (i === 0) {
-        past.a.href = "?page=2&max_id=" + s.id;
-      }
-      var li = ce("li");
-      [s.user.screen_name, s.text, s.created_at].forEach(function(s) {
-        var p = ce("p");
-        p.appendChild(ct(s));
-        li.appendChild(p);
+        ul.appendChild(li);
       });
-      ul.appendChild(li);
+
+      document.body.appendChild(makeCursor(data));
+      document.body.appendChild(ul);
     });
-    ul.appendChild(past);
-    document.body.appendChild(ul);
   };
 
 
 
 
 
-  initDOM();
-  get("/1/statuses/user_timeline.json?count=1", getMyself);
+  function showTL(u, my) {
+    get(u, function(xhr) {
+      var TL = JSON.parse(xhr.responseText);
+      var list = ce("ol");
+      list.id = "timeline";
+
+      var past = ce("li");
+      past.a = ce("a");
+
+      TL.forEach(function(s, i) {
+        var li = ce("li");
+        [s.user.screen_name, s.text, s.created_at].forEach(function(s) {
+          var p = ce("p");
+          p.appendChild(ct(s));
+          li.appendChild(p);
+        });
+        list.appendChild(li);
+      });
+
+      document.body.appendChild(list);
+
+      if (TL.length) {
+        past.a.appendChild(ct("past"));
+        past.appendChild(past.a);
+        past.a.href = "?page=2&max_id=" + TL[0].id;
+        document.body.appendChild(past);
+      }
+    });
+  };
+
+
+
+
+
+  function main() {
+    if (~document.cookie.indexOf("auth_token")) {
+      get("/1/statuses/user_timeline.json?count=1", function(xhr) {
+        var my = JSON.parse(xhr.responseText)[0].user;
+        initDOM(my);
+        getPage(my);
+      });
+    } else {
+      location =
+      "/login?redirect_after_login=http%3A%2F%2Fapi.twitter.com%2F%2B%2F";
+    }
+  };
+
+
+
+
+  main();
 
 }, false);
