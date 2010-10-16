576883cf318aac72cc7670f1f55d963a0d6e923e
0mg <ray.0mg.ppppp@gmail.com>
2010-10-16 13:44:56 +0900

    Added New Script --- Tumblr Thumbnail Larger

---
+++ tbThumb.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name Tumblr Thumbnail Larger
+// @include http://www.tumblr.com/dashboard
+// @include http://www.tumblr.com/dashboard/*
+// ==/UserScript==
+
+addEventListener("DOMContentLoaded", function() {
+  Array.prototype.forEach.call(document.getElementsByTagName("img"),
+  function(thumb) {
+    if (thumb.className === "image_thumbnail") thumb.onclick();
+  });
+}, false);
