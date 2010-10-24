4d6bbb9a67ab0f9f9bfd26747448da29058bd308
0mg <ray.0mg.ppppp@gmail.com>
2010-10-24 19:49:58 +0900

    updated

--- pixivid.js
+++ pixivid.js
@@ -3,24 +3,23 @@
 // @include http://www.pixiv.net/*
 // ==/UserScript==
 
-window.addEventListener("DOMContentLoaded", function() {
-  var contents = document.getElementById("contents");
-  if (!contents) return;
-  var icon = contents.getElementsByTagName("img")[0];
-  if (!icon) return;
-  var ul = contents.getElementsByTagName("ul")[0];
-  if (!ul) return;
-  var key = "/profile/";
-  if (icon.src.indexOf(key) === -1) return;
-  var id = icon.src.split(key)[1].split("/")[0];
-  var li = document.createElement("li");
-  var a = document.createElement("a");
-  a.href = "http://drawr.net/" + id;
-  a.style.display = "inline-block";
-  a.style.paddingLeft = "24px";
-  a.style.background =
-  'url("http://drawr.net/images/icon_top.gif") no-repeat left center';
-  a.appendChild(document.createTextNode(id));
-  li.appendChild(a);
-  ul.appendChild(li);
+addEventListener("DOMContentLoaded", function() {
+  var id =
+  document.evaluate('.//a[@class="avatar_m"]/img/@src[contains(.,"/profile/")]',
+  document, null, 2, null).stringValue;
+  var menu = document.evaluate('.//ul[@class="person_menu"]',
+  document, null, 9, null).singleNodeValue;
+  if (id && menu) {
+    id = id.match(/\/profile\/([^/]+)/)[1];
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    a.href = "http://drawr.net/" + id;
+    a.style.display = "inline-block";
+    a.style.paddingLeft = "24px";
+    a.style.background =
+    'url("http://drawr.net/images/icon_top.gif") no-repeat left center';
+    a.appendChild(document.createTextNode(id));
+    li.appendChild(a);
+    menu.appendChild(li);
+  }
 }, false);
