dbc556b51caed1b507303c4ab0fe803a9d68332c
0mg <ray.0mg.ppppp@gmail.com>
2010-10-25 17:01:11 +0900

    updated

--- pixivid.js
+++ pixivid.js
@@ -4,22 +4,26 @@
 // ==/UserScript==
 
 addEventListener("DOMContentLoaded", function() {
-  var profsrc =
-  document.evaluate('.//a[@class="avatar_m"]/img/@src[contains(.,"/profile/")]',
-  document, null, 2, null).stringValue;
-  var menu = document.evaluate('.//ul[@class="person_menu"]',
-  document, null, 9, null).singleNodeValue;
+  var profsrc = document.evaluate('.//div[@class="extaraNavi"]//' +
+    '@href[starts-with(.,"http://www.pixiv.net/stacc/")]',
+    document, null, 2, null).stringValue;
+  var menu = document.evaluate('.//ul[@class="person_menu"]', document, null,
+    9, null).singleNodeValue;
+
   if (profsrc && menu) {
-    var id = profsrc.match(/\/profile\/([^/]+)/)[1];
+    var id = profsrc.split("/").pop();
     var li = document.createElement("li");
     var a = document.createElement("a");
+
     a.href = "http://drawr.net/" + id;
     a.style.display = "inline-block";
     a.style.paddingLeft = "24px";
     a.style.background =
     'url("http://drawr.net/images/icon_top.gif") no-repeat left center';
+
     a.appendChild(document.createTextNode(id));
     li.appendChild(a);
     menu.appendChild(li);
   }
+
 }, false);
