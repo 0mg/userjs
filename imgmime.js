f2d76b951e65e80bcfaecb986cef3beb53dd144e
0mg <ray.0mg.ppppp@gmail.com>
2010-11-02 05:47:33 +0900

    updated

--- imgmime.js
+++ imgmime.js
@@ -1,10 +1,25 @@
 // ==UserScript==
-// @name Yahoo! blog image MIME type fix
-// @include http://img*.blogs.yahoo.co.jp/ybi/*
+// @name image MIME type fix
+// @include *
 // ==/UserScript==
 
 addEventListener("DOMContentLoaded", function() {
-  var img = document.createElement("img");
-  img.src = location.href;
-  document.body.replaceChild(img, document.body.firstChild);
+  if (document.body && document.body.childNodes.length === 1) {
+    if (
+      document.body.lastChild.nodeName === "#text" ||
+      document.body.lastChild.nodeName.toLowerCase() === "pre"
+    ) {
+      var img = document.createElement("img");
+      img.src = location.href;
+      img.width = "0";
+      document.body.appendChild(img);
+      alert(img.height);
+      if (img.height === 1) {
+        document.body.removeChild(document.body.firstChild);
+        img.removeAttribute("width");
+      } else {
+        document.body.removeChild(document.body.lastChild);
+      }
+    }
+  }
 }, false);
