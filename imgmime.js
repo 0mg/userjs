13bdaebf8aa8fe5620c33259092e7dd5b067d1f8
0mg <ray.0mg.ppppp@gmail.com>
2010-11-02 06:48:55 +0900

    updated

--- imgmime.js
+++ imgmime.js
@@ -3,28 +3,21 @@
 // @include *
 // ==/UserScript==
 
-/*
-  開発メモ：ローカルファイル未対応
-  対応させたい
-*/
-
 document.body && addEventListener("DOMContentLoaded", function() {
-  if (document.body.childNodes.length === 1) {
+  if (document.body && document.body.childNodes.length === 1) {
     if (
       document.body.lastChild.nodeName === "#text" ||
-      document.body.lastChild.nodeName.toUpperCase() === "PRE"
+      document.body.lastChild.nodeName.toLowerCase() === "pre"
     ) {
       var img = document.createElement("img");
       img.src = location.href;
-      if (location.protocol !== "file:") {
-        img.onload = function() {
-          document.body.removeChild(document.body.firstChild);
-          img.removeAttribute("width");
-        };
-        img.onerror = function() {
-          document.body.removeChild(document.body.lastChild);
-        };
-        document.body.appendChild(img);
+      img.width = "0";
+      document.body.appendChild(img);
+      if (img.height === 1) {
+        document.body.removeChild(document.body.firstChild);
+        img.removeAttribute("width");
+      } else {
+        document.body.removeChild(document.body.lastChild);
       }
     }
   }
