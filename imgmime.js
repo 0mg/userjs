4a2961189d82e6d8ce73c4287019556e4d1cdb98
0mg <ray.0mg.ppppp@gmail.com>
2010-12-27 22:34:15 +0900

    updated

--- imgmime.js
+++ imgmime.js
@@ -4,7 +4,8 @@
 // ==/UserScript==
 
 document.body && addEventListener("DOMContentLoaded", function() {
-  if (document.body && document.body.childNodes.length === 1) {
+  if (document.body && document.body.childNodes.length <= 2) {
+    // binary may contains unknown HTML element. like <dfghj>
     if (
       document.body.firstChild.nodeName === "#text" ||
       document.body.firstChild.nodeName.toLowerCase() === "pre"
@@ -12,9 +13,11 @@ document.body && addEventListener("DOMContentLoaded", function() {
       var img = document.createElement("img");
       img.src = location.href;
       img.width = 0;
-      document.body.insertBefore(img, document.body.firstChild);
+      document.body.appendChild(img);
       if (img.height === 1) { // img.onload
-        document.body.removeChild(document.body.lastChild);
+        while (document.body.childNodes.length > 1) {
+          document.body.removeChild(document.body.firstChild);
+        }
         img.removeAttribute("width");
       } else { // img.onerror
         img.parentNode.removeChild(img);
