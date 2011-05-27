4eb31e1f3950c07e42de19790bb13918eaf0f6b6
0mg <ray.0mg.ppppp@gmail.com>
2011-05-27 11:00:16 +0900

    updated

--- hatebu.js
+++ hatebu.js
@@ -2,7 +2,7 @@
 // @include http://b.hatena.ne.jp/*
 // ==/UserScript==
 
-// URL に # 付きのはてブを表示するとキーボードの入力が受け付けられなくなる Opera 9.64 特有のバグを解消する
+// Opera 9.64 「はてブを表示するとキーボードの入力が受け付けられなくなることがある」バグを解消する
 addEventListener("DOMContentLoaded", function() {
 	delete Hatena.Bookmark;
 }, false);
