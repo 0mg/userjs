fcb3b23798fa28275f68b14e57abd63558208a48
0mg <ray.0mg.ppppp@gmail.com>
2011-07-01 15:20:21 +0900

    ren: ggsugest.js

--- ggsugest.js
+++ ggsugest.js
@@ -2,7 +2,6 @@
 // @name Google Fixes for Opera 9.64
 // ==/UserScript==
 
-// Fixes for Suggest
 addEventListener("keypress", function(v) {
   if (v.target.autocomplete && (v.keyCode === 38 || v.keyCode === 40)) {
     if (v.shiftKey) v.stopPropagation();
