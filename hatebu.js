01ba335b8065815d2d9b76df6711174005f091b1
0mg <ray.0mg.ppppp@gmail.com>
2011-08-07 20:40:19 +0900

    fix: only 9.64

--- hatebu.js
+++ hatebu.js
@@ -3,6 +3,6 @@
 // ==/UserScript==
 
 // Opera 9.64 「はてブを表示するとキーボードの入力が受け付けられなくなることがある」バグを解消する
-addEventListener("DOMContentLoaded", function() {
+opera.version() === "9.64" && addEventListener("DOMContentLoaded", function() {
 	delete Hatena.Bookmark;
 }, false);
