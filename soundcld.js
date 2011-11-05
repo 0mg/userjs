f4ac43c140a8621acb6e0833b27d600d20e77030
0mg <ray.0mg.ppppp@gmail.com>
2011-11-05 11:00:08 +0900

    fix: show links with correct order

--- soundcld.js
+++ soundcld.js
@@ -3,41 +3,43 @@
 // ==/UserScript==
 
 addEventListener("DOMContentLoaded", function() {
-	var tracks = window.SC.clientDB.getTracks();
-	var wrapper = document.createElement("div");
-	for (var i in tracks) {
-		var track = tracks[i];
-		var a = document.createElement("a");
-		a.href = track.streamUrl;
-		a.textContent = track.title;
-		wrapper.appendChild(a);
-	}
-	if (wrapper.hasChildNodes()) {
-		decorate(wrapper);
-		append(wrapper) ||
-		document.body.insertBefore(wrapper, document.body.firstChild);
-	}
-	function decorate(wrapper) {
-		for (var i = 0; i < wrapper.childNodes.length; ++i) {
-			var a = wrapper.childNodes[i];
-			a.style.display = "inline-block";
-			a.className = "download icon-button";
-			var span = document.createElement("span");
-			span.appendChild(a.lastChild);
-			a.appendChild(span);
-		}
-		var clearfix = document.createElement("br");
-		clearfix.style.clear = "both";
-		wrapper.appendChild(clearfix);
-		wrapper.id = "userjs-extra-download-links";
-	}
-	function append(wrapper) {
-		var append_pos = document.getElementById("main-content-inner");
-		if (!append_pos) return false;
-		var prepend = true;
-		prepend ?
-		append_pos.insertBefore(wrapper, append_pos.firstChild) :
-		append_pos.appendChild(wrapper);
-		return true;
-	}
+  var _tracks = window.SC.clientDB.getTracks();
+  var tracks = [];
+  for (var i in _tracks) { // _tracks.reverse()
+    tracks.unshift(_tracks[i]);
+  }
+  _tracks = null;
+  var wrapper = document.createElement("div");
+  for (var i = 0; i < tracks.length; ++i) {
+    var track = tracks[i];
+    var a = document.createElement("a");
+    a.href = track.streamUrl;
+    a.textContent = track.title;
+    wrapper.appendChild(a);
+  }
+  if (wrapper.hasChildNodes()) {
+    decorate(wrapper);
+    append(wrapper) ||
+    document.body.insertBefore(wrapper, document.body.firstChild);
+  }
+  function decorate(wrapper) {
+    for (var i = 0; i < wrapper.childNodes.length; ++i) {
+      var a = wrapper.childNodes[i];
+      a.style.display = "inline-block";
+      a.className = "download icon-button";
+      var span = document.createElement("span");
+      span.appendChild(a.lastChild);
+      a.appendChild(span);
+    }
+    wrapper.id = "userjs-extra-download-links";
+  }
+  function append(wrapper) {
+    var append_pos = document.getElementById("main-content-inner");
+    if (!append_pos) return false;
+    var prepend = true;
+    prepend ?
+    append_pos.insertBefore(wrapper, append_pos.firstChild) :
+    append_pos.appendChild(wrapper);
+    return true;
+  }
 }, false);
