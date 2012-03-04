e881b13b4577f5ca61206125e6b8a9c126000c75
0mg <0millig@gmail.com>
2012-03-04 14:32:40 +0900

    new: open page later

---
+++ later.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @include *
+// ==/UserScript==
+
+addEventListener("keyup", (function() {
+  var nthPress = 0, timer = null;
+  return function(ev) {
+    if (ev.target instanceof HTMLInputElement ||
+        ev.target instanceof HTMLTextAreaElement) return;
+    if (ev.keyCode === 76) { // 108, 76: L
+      if (nthPress) {
+        var url = location.href.replace(/'/g, "%2527");
+        var title = String(document.title).replace(/'/g, "%2527");
+        open("javascript:alert('" + title + "%5Cn" + url +
+          "'),location='" + url + "'");
+        close();
+      } else {
+        ++nthPress;
+        clearTimeout(timer);
+        timer = setTimeout(function() { doublePress = 0; }, 180);
+      }
+    }
+  };
+})(), true);
