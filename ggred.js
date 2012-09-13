1c1733aaac6df294ba3fbf080b52a616fa570799
0mg <0millig@gmail.com>
2012-09-13 17:13:16 +0900

    new: google redirect.js

---
+++ ggred.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @include http://www.google.com/url?*
+// @include https://www.google.com/url?*
+// @include http://www.google.co.jp/url?*
+// @include https://www.google.co.jp/url?*
+// ==/UserScript==
+location.replace(
+  decodeURIComponent(location.search.match(/[?&](?:url|q)=([^&]+)/)[1])
+);
