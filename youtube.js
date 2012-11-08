1eeb3e01ea6da7cada7f690b0344bb794362dd08
0mg <0millig@gmail.com>
2012-11-08 17:09:25 +0900

    new: youtube.js

---
+++ youtube.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name add YouTube URLs to playlist
+// @include http://www.youtube.com/playlist?action_edit=1&list=*
+// @exclude 
+// ==/UserScript==
+
+addEventListener("DOMContentLoaded", function() {
+  var adds = function() {};
+
+  var addbtn = document.querySelector(".playlist-add-video-url-button-add");
+  addbtn.addEventListener("DOMAttrModified", function fun(e) {
+    if (!e.target.disabled) {
+      adds();
+    }
+  }, false);
+
+  var input = document.querySelector(".add-video-panel-url");
+
+  var el = document.createElement("textarea");
+  var el2 = document.createElement("button");
+
+  el2.addEventListener("click", function(e) {
+    e.preventDefault();
+    adds = function() {
+      var urls = el.value.split(/\r\n|\r|\n|,/);
+      if (urls.join("")) {
+        var url = urls.shift();
+        var ev = document.createEvent("MouseEvent");
+        ev.initEvent("click", true, false);
+        el.value = urls;
+        input.value = url;
+        addbtn.dispatchEvent(ev);
+      } else {
+        adds = function() {};
+      }
+    };
+    adds();
+  }, false);
+  el2.textContent = "add all";
+
+  var eldst = document.querySelector("#add-playlist-video-dialog");
+  eldst.appendChild(el);
+  eldst.appendChild(el2);
+}, false);
