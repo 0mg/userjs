cd316ea4ec46d71c2efaf7f7337432a5074ba4f7
0mg <0millig@gmail.com>
2013-03-02 15:45:41 +0900

    fix: workable

--- pixivid.js
+++ pixivid.js
@@ -4,26 +4,20 @@
 // ==/UserScript==
 
 addEventListener("DOMContentLoaded", function() {
-  var profsrc = document.evaluate('.//div[@class="extaraNavi"]//' +
-    '@href[starts-with(.,"http://www.pixiv.net/stacc/")]',
-    document, null, 2, null).stringValue;
-  var menu = document.evaluate('.//ul[@class="person_menu"]', document, null,
-    9, null).singleNodeValue;
-
-  if (profsrc && menu) {
-    var id = profsrc.substring(27).match(/[-_a-z0-9]+/);
+  var src =
+    document.querySelector("ul.tabs a[href^='http://www.pixiv.net/stacc/']");
+  var dst = document.querySelector(".user-relation");
+  if (src && dst) {
+    var id = src.href.substring(27);
     var li = document.createElement("li");
     var a = document.createElement("a");
-
     a.href = "http://drawr.net/" + id;
     a.style.display = "inline-block";
     a.style.paddingLeft = "24px";
     a.style.background =
-    'url("http://drawr.net/images/icon_top.gif") no-repeat left center';
-
-    a.appendChild(document.createTextNode(id));
+      "url(http://drawr.net/images/icon_top.gif) no-repeat left";
+    a.textContent = id;
     li.appendChild(a);
-    menu.appendChild(li);
+    dst.appendChild(li);
   }
-
-}, false);
+}, true);
