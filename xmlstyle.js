d3158b155b15ef760481c61874cbb703dea769e8
0mg <0millig@gmail.com>
2013-03-02 12:31:09 +0900

    upd: visible doctype

--- xmlstyle.js
+++ xmlstyle.js
@@ -22,35 +22,79 @@ addEventListener("DOMContentLoaded", function() {
     return nodeModel;
   }
 
+  var genDoctype = function(targetNode) {
+    if (!targetNode) return document.createDocumentFragment();
+    var pub
+    var nodeModel = {
+      root: ce("doctype"),
+      stag: {
+        root: ce("tag"),
+        name: ce("name"),
+        exid: ce("exid"),
+        pubid: ce("pubid"),
+        sysid: ce("sysid")
+      },
+    };
+    nodeModel.stag.name.appendChild(ct(targetNode.name));
+
+    nodeModel.stag.exid.appendChild(ct(
+      targetNode.publicId ? "PUBLIC" : targetNode.systemId ? "SYSTEM" : ""
+    ));
+    if (targetNode.publicId) {
+      nodeModel.stag.pubid.appendChild(ct('"' + targetNode.publicId + '"'));
+    }
+    if (targetNode.systemId) {
+      nodeModel.stag.sysid.appendChild(ct('"' + targetNode.systemId + '"'));
+    }
+
+    nodeModel.stag.root.appendChild(ct("<!DOCTYPE"));
+    nodeModel.stag.root.appendChild(nodeModel.stag.name);
+    nodeModel.stag.root.appendChild(nodeModel.stag.exid);
+    nodeModel.stag.root.appendChild(nodeModel.stag.pubid);
+    nodeModel.stag.root.appendChild(nodeModel.stag.sysid);
+    nodeModel.stag.root.appendChild(ct(">"));
+    nodeModel.root.appendChild(nodeModel.stag.root);
+
+    return nodeModel.root;
+  };
+
+  var genNodeModelProc = function(targetNode) {
+    var nodeModel = {
+      root: ce("proc"),
+      stag: {
+        root: ce("tag"),
+        name: ce("name"),
+        value: ce("value")
+      }
+    };
+    nodeModel.stag.root.appendChild(ct("<?"));
+    nodeModel.stag.name.appendChild(ct(targetNode.nodeName));
+    nodeModel.stag.root.appendChild(nodeModel.stag.name);
+    if (targetNode.nodeValue) {
+      nodeModel.stag.value.appendChild(ct(targetNode.nodeValue));
+      nodeModel.stag.root.appendChild(nodeModel.stag.value);
+    }
+    nodeModel.stag.root.appendChild(ct("?>"));
+    nodeModel.root.appendChild(nodeModel.stag.root);
+    return nodeModel.root;
+  };
+
   function genNodeModel(targetNode) {
     switch (targetNode.nodeType) {
 
-    case document.PROCESSING_INSTRUCTION_NODE:
+    case document.DOCUMENT_NODE:
       var nodeModel = {
-        parent: ce("proc"),
-        stag: {
-          parent: ce("tag"),
-          name: ce("name"),
-          value: ce("value")
-        }
+        root: ce("document"),
+        children: ce("children")
       };
+      nodeModel.root.appendChild(nodeModel.children);
+      return nodeModel.root;
 
-      nodeModel.stag.parent.appendChild(ct("<?"));
+    case document.DOCUMENT_TYPE_NODE:
+      return genDoctype(targetNode);
 
-      nodeModel.stag.name.appendChild(ct(targetNode.nodeName));
-      nodeModel.stag.parent.appendChild(nodeModel.stag.name);
-
-      if (targetNode.nodeValue) {
-        nodeModel.stag.value.appendChild(ct(targetNode.nodeValue));
-        nodeModel.stag.parent.appendChild(nodeModel.stag.value);
-      }
-
-      nodeModel.stag.parent.appendChild(ct("?>"));
-
-      nodeModel.parent.appendChild(nodeModel.stag.parent);
-
-      return nodeModel.parent;
-
+    case document.PROCESSING_INSTRUCTION_NODE:
+      return genNodeModelProc(targetNode);
 
     case document.TEXT_NODE:
       var nodeModel = ce("text");
@@ -60,120 +104,133 @@ addEventListener("DOMContentLoaded", function() {
 
     case document.CDATA_SECTION_NODE:
       var nodeModel = {
-        parent: ce("cdata"),
+        root: ce("cdata"),
         stag: {
-          parent: ce("tag"),
+          root: ce("tag"),
           name: ce("name")
         },
         value: ce("value"),
         etag: ce("tag")
       };
 
-      nodeModel.stag.parent.appendChild(ct("<!["));
+      nodeModel.stag.root.appendChild(ct("<!["));
       nodeModel.stag.name.appendChild(ct("CDATA"));
-      nodeModel.stag.parent.appendChild(nodeModel.stag.name);
-      nodeModel.stag.parent.appendChild(ct("["));
-      nodeModel.parent.appendChild(nodeModel.stag.parent);
+      nodeModel.stag.root.appendChild(nodeModel.stag.name);
+      nodeModel.stag.root.appendChild(ct("["));
+      nodeModel.root.appendChild(nodeModel.stag.root);
 
       nodeModel.value.appendChild(ct(targetNode.nodeValue));
-      nodeModel.parent.appendChild(nodeModel.value);
+      nodeModel.root.appendChild(nodeModel.value);
 
       nodeModel.etag.appendChild(ct("]]>"));
-      nodeModel.parent.appendChild(nodeModel.etag);
+      nodeModel.root.appendChild(nodeModel.etag);
 
-      return nodeModel.parent;
+      return nodeModel.root;
 
 
     case document.COMMENT_NODE:
       var nodeModel = {
-        parent: ce("comment"),
+        root: ce("comment"),
         stag: ce("tag"),
         value: ce("value"),
         etag: ce("tag")
       };
       nodeModel.stag.appendChild(ct("<!--"));
-      nodeModel.parent.appendChild(nodeModel.stag);
+      nodeModel.root.appendChild(nodeModel.stag);
 
       nodeModel.value.appendChild(ct(targetNode.nodeValue));
-      nodeModel.parent.appendChild(nodeModel.value);
+      nodeModel.root.appendChild(nodeModel.value);
 
       nodeModel.etag.appendChild(ct("-->"));
-      nodeModel.parent.appendChild(nodeModel.etag);
+      nodeModel.root.appendChild(nodeModel.etag);
 
-      return nodeModel.parent;
+      return nodeModel.root;
 
 
     case document.ELEMENT_NODE:
       var nodeModel = {
-        parent: ce("element"),
+        root: ce("element"),
         stag: {
-          parent: ce("tag"),
+          root: ce("tag"),
           name: ce("name")
         },
         children: ce("children"),
         etag: {
-          parent: ce("tag"),
+          root: ce("tag"),
           name: ce("name")
         }
       };
-      nodeModel.stag.parent.appendChild(ct("<"));
+      nodeModel.stag.root.appendChild(ct("<"));
       nodeModel.stag.name.appendChild(ct(targetNode.nodeName));
-      nodeModel.stag.parent.appendChild(nodeModel.stag.name);
+      nodeModel.stag.root.appendChild(nodeModel.stag.name);
 
       for (var i = 0, l = targetNode.attributes.length; i < l; ++i) {
         var targetAttr = targetNode.attributes[i];
 
         // attr 'role' is invisible in Opera 9.6
-        if (targetAttr === void null) break;
+        if (targetAttr === undefined) break;
 
         var attrModel = {
-          parent: ce("attr"),
+          root: ce("attr"),
           name: ce("name"),
           value: ce("value")
         };
         attrModel.name.appendChild(ct(targetAttr.nodeName));
         attrModel.value.appendChild(ct(targetAttr.nodeValue));
 
-        attrModel.parent.appendChild(attrModel.name);
-        attrModel.parent.appendChild(ct('="'));
-        attrModel.parent.appendChild(attrModel.value);
-        attrModel.parent.appendChild(ct('"'));
+        attrModel.root.appendChild(attrModel.name);
+        attrModel.root.appendChild(ct('="'));
+        attrModel.root.appendChild(attrModel.value);
+        attrModel.root.appendChild(ct('"'));
 
-        nodeModel.stag.parent.appendChild(attrModel.parent);
+        nodeModel.stag.root.appendChild(attrModel.root);
       }
 
       if (targetNode.hasChildNodes()) {
-        nodeModel.stag.parent.appendChild(ct(">"));
-        nodeModel.parent.appendChild(nodeModel.stag.parent);
+        nodeModel.stag.root.appendChild(ct(">"));
+        nodeModel.root.appendChild(nodeModel.stag.root);
 
-        nodeModel.parent.appendChild(nodeModel.children);
+        nodeModel.root.appendChild(nodeModel.children);
 
         nodeModel.etag.name.appendChild(ct(targetNode.nodeName));
-        nodeModel.etag.parent.appendChild(ct("</"));
-        nodeModel.etag.parent.appendChild(nodeModel.etag.name);
-        nodeModel.etag.parent.appendChild(ct(">"));
-        nodeModel.parent.appendChild(nodeModel.etag.parent);
+        nodeModel.etag.root.appendChild(ct("</"));
+        nodeModel.etag.root.appendChild(nodeModel.etag.name);
+        nodeModel.etag.root.appendChild(ct(">"));
+        nodeModel.root.appendChild(nodeModel.etag.root);
 
       } else {
-        nodeModel.stag.parent.appendChild(ct("/>"));
+        nodeModel.stag.root.appendChild(ct("/>"));
 
-        nodeModel.parent.appendChild(nodeModel.stag.parent);
+        nodeModel.root.appendChild(nodeModel.stag.root);
       }
 
-      return nodeModel.parent;
+      return nodeModel.root;
+
+    default:
+      var nodeModel = {
+        root: ce("unknown"),
+        data: ce("value"),
+        children: ce("children")
+      };
+      nodeModel.data.appendChild(
+        ct(XMLSerializer().serializeToString(targetNode)));
+      nodeModel.root.appendChild(nodeModel.data);
+      nodeModel.root.appendChild(nodeModel.children);
+
+      return nodeModel.root;
     }
   }
 
   var nsuri = "http://www.w3.org/1999/xhtml";
 
   var nodeModel = {
-    parent: ce("root"),
+    root: ce("root"),
     title: document.createElementNS(nsuri, "title"),
     style: document.createElementNS(nsuri, "style")
   };
 
   nodeModel.title.appendChild(ct(decodeURI(document.documentURI)));
-  nodeModel.parent.appendChild(nodeModel.title);
+  nodeModel.root.appendChild(nodeModel.title);
 
   nodeModel.style.appendChild(ct('\
     :root {\
@@ -182,35 +239,40 @@ addEventListener("DOMContentLoaded", function() {
       background-color: #fcfcfc;\
       font-family: monospace;\
     }\
-    proc, comment, element, children, cdata, text {\
+    document, doctype, proc, comment, element, children, cdata, text {\
       display: block;\
     }\
     children {\
       padding-left: 2ex;\
     }\
-    tag name, attr name, proc name {\
+    tag>name, attr>name, proc>name {\
       color: blue;\
     }\
-    attr name, proc value {\
+    attr>name, proc>tag>value, doctype>tag>name, exid, pubid, sysid {\
       margin-left: 1ex;\
     }\
-    attr value, proc value {\
+    exid:empty, pubid:empty, sysid:empty {\
+      display: none;\
+    }\
+    attr>value, proc>tag>value, pubid, sysid {\
       color: green;\
     }\
-    /*text, cdata value, comment value {\
+    /*text, cdata>value, comment>value {\
       white-space: pre;\
     }*/\
-    text, cdata value {\
+    text, cdata>value {\
       color: red;\
     }\
-    comment value {\
-      color: silver;\
+    comment>value {\
+      color: gray;\
     }\
+    unknown>value {\
+      font-style: italic;\
+      color: #f0f;\
+    }\
+    .hidden{display:none}\
   '));
-  nodeModel.parent.appendChild(nodeModel.style);
-
-  nodeModel.parent.appendChild(genNodeTreeModel(document.documentElement));
-
-  document.replaceChild(nodeModel.parent, document.documentElement);
-
-}, false);
+  nodeModel.root.appendChild(nodeModel.style);
+  nodeModel.root.appendChild(genNodeTreeModel(document));
+  document.replaceChild(nodeModel.root, document.documentElement);
+}, true);
