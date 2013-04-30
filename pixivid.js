028072fd4fb0541185e21063e4f212ad95d6ddea
0mg <0millig@gmail.com>
2013-04-30 13:13:14 +0900

    fix: remove ?query=string from user ID item

--- pixivid.js
+++ pixivid.js
@@ -3,21 +3,30 @@
 // @include http://www.pixiv.net/*
 // ==/UserScript==
 
-addEventListener("DOMContentLoaded", function() {
-  var src =
-    document.querySelector("ul.tabs a[href^='http://www.pixiv.net/stacc/']");
-  var dst = document.querySelector(".user-relation");
-  if (src && dst) {
-    var id = src.href.substring(27);
-    var li = document.createElement("li");
-    var a = document.createElement("a");
-    a.href = "http://drawr.net/" + id;
-    a.style.display = "inline-block";
-    a.style.paddingLeft = "24px";
-    a.style.background =
-      "url(http://drawr.net/images/icon_top.gif) no-repeat left";
-    a.textContent = id;
-    li.appendChild(a);
-    dst.appendChild(li);
-  }
-}, true);
+;(function() {
+  "use strict";
+  var addUIDNode = function() {
+    var KEY_URL = "http://www.pixiv.net/stacc/";
+    var NODE_ID = "userjs-user-id-item";
+    var src = document.querySelector("ul.tabs a[href^='" + KEY_URL + "']");
+    var dst = document.querySelector(".user-relation");
+    var uidnode = document.querySelector("#" + NODE_ID);
+    if (!uidnode && src && dst) {
+      var userID = src.href.slice(KEY_URL.length).match(/[-\w]*/)[0];
+      var li = document.createElement("li");
+      var a = document.createElement("a");
+      li.id = NODE_ID;
+      a.textContent = userID;
+      a.href = "http://drawr.net/" + userID;
+      a.style.display = "inline-block";
+      a.style.paddingLeft = "24px";
+      a.style.backgroundImage = "url(http://drawr.net/images/icon_top.gif)";
+      a.style.backgroundRepeat = "no-repeat";
+      li.appendChild(a);
+      dst.appendChild(li);
+    }
+  };
+  if (document.readyState === "complete") addUIDNode();
+  else addEventListener("DOMContentLoaded", addUIDNode);
+  addEventListener("popstate", addUIDNode);
+})();
