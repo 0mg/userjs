d1354c489ada1fb56b02f1e11e242ecfc0513812
0mg <0millig@gmail.com>
2013-05-01 21:41:33 +0900

    fix: functionable

--- tumblr.js
+++ tumblr.js
@@ -1,40 +1,11 @@
 // ==UserScript==
-// @name Tumblr
 // @include http://www.tumblr.com/*
 // ==/UserScript==
 
-/* resize larger thumbnail */
 addEventListener("DOMContentLoaded", function() {
-  Array.prototype.forEach.call(document.getElementsByTagName("img"),
-  function(thumb) {
-    if (thumb.onclick && thumb.className === "image_thumbnail") {
-      thumb.onclick();
-      /* link to original image */
-      thumb.onclick = function() {
-        location.href =
-        document.getElementById(thumb.id.replace(/\D+/, "photo_info_")).
-        getElementsByTagName("a")[0].href;
-      };
-    }
+  [].forEach.call(document.querySelectorAll(".image_thumbnail"), function(img) {
+    var ce = document.createEvent("Event");
+    ce.initEvent("click", true, true);
+    img.dispatchEvent(ce);
   });
-}, false);
-
-/* dont scroll top when liked post */
-addEventListener("DOMContentLoaded", function() {
-  Array.prototype.forEach.call(document.links, function(a) {
-    if ((" " + a.className + " ").indexOf(" like_button ") >= 0) {
-    	a.href = "javascript:;";
-    }
-  });
-}, false);
-
-/* link for Opera fast forward */
-addEventListener("DOMContentLoaded", function() {
-  var nexta = document.getElementById("next_page_link");
-  if (nexta) {
-    var link = document.createElement("link");
-    link.rel = "next";
-    link.href = nexta.href;
-    document.head.appendChild(link);
-  }
-}, false);
+});
