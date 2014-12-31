1da27e2256351d0cec73d8472f4f08cd28c37546
0mg <millig@outlook.jp>
2014-12-31 15:41:58 +0900

    dev: mirror to Greasy Fork's same one

--- twImgOrig.user.js
+++ twImgOrig.user.js
@@ -2,8 +2,8 @@
 // @name OpenTweetImageOrig
 // @description ツイート画像をクリックで原寸大表示します. (Click to open tweet image as original size)
 // @version 1.0
-// @updateURL https://github.com/0mg/userjs/raw/master/twImgOrig.user.js
 // @include https://twitter.com/*
+// @namespace https://greasyfork.org/users/8032
 // ==/UserScript==
 
 window.addEventListener("click", function(event) {
