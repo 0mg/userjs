5aae2720f66d93dd0385ab1edd72b2aa8be8f76f
0mg <millig@outlook.jp>
2016-02-22 19:32:16 +0900

    upd: notify with yellow box if not exist new broadcast

--- livenico.js
+++ livenico.js
@@ -18,9 +18,27 @@ addEventListener("dblclick", function() {
   xhr.addEventListener("load", function(e) {
     var xhr = e.target;
     if (xhr.status !== 200) return alert("通知ボックス取得失敗");
-    var re = RegExp('<a href="(.+)"[^<]+<.+title="' + commuName + '"');
+    var re = RegExp('<a href="(.+)"[^<]+<.+title="' +
+      commuName.replace(/[\\^$*+?.()|:{},]/g, "\\$&") + '"');
     var ma = xhr.responseText.match(re);
-    if (!ma) return alert("次枠ないです\nマッチしませんでした:" + re);
+    // 次枠がない場合
+    if (!ma) {
+      var notify = document.createElement("p");
+      notify.style.position = "fixed";
+      notify.style.zIndex = "9999";
+      notify.style.backgroundColor = "yellow";
+      notify.style.fontSize = "larger";
+      notify.style.bottom = "0%";
+      notify.style.left = "0";
+      notify.style.whiteSpace = "pre-wrap";
+      notify.textContent = "次枠ないです\nマッチしませんでした\n" + re;
+      document.body.appendChild(notify);
+      setTimeout(function() {
+        notify.parentNode.removeChild(notify);
+      }, 1000);
+      return;
+    }
+    // 次枠がある場合
     if (ma[1]) location.href = ma[1];
   });
   xhr.send();
