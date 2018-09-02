08737bfc91c211b441428d2b6a2553dd746f8631
0mg <millig@outlook.jp>
2018-09-02 13:11:37 +0900

    fix: tumbphot.js for Vivaldi

--- tumbphot.js
+++ tumbphot.js
@@ -1,9 +1,11 @@
 // ==UserScript==
+// @include http://*.tumblr.com/private/*
 // @include http://*.tumblr.com/*/photoset_iframe/*
 // ==/UserScript==
 
 addEventListener("DOMContentLoaded", function() {
-  // add link(==location.href) to body
+  // # add link(==location.href) to body
+  // * pattern 1
   if (parent !== self) {
     var a = document.createElement("a");
     a.href = location;
@@ -12,8 +14,20 @@ addEventListener("DOMContentLoaded", function() {
     document.body.insertBefore(a, document.body.firstChild);
     return;
   }
+  // * pattern 2
+  if (location.href.includes("/private/")) {
+    var iframe = document.querySelector(".photoset");
+    if (iframe) {
+      var a = document.createElement("a");
+      a.href = iframe.src;
+      a.textContent = a.href;
+      a.target = "_parent";
+      document.body.insertBefore(a, document.body.firstChild);
+    }
+    return;
+  }
 
-  // show all photoset original urls into page end
+  // # show all photoset original urls into page end
   var md = "";
   [].forEach.call(document.links, function(a) {
     md += "![](" + a.href + ")\n\n";
@@ -24,13 +38,13 @@ addEventListener("DOMContentLoaded", function() {
   dst.value = md;
   document.body.appendChild(dst);
 
-  // show all photoset original images
+  // # show all photoset original images
   [].forEach.call(document.querySelectorAll("*"), function(e) {
     if (e.tagName === "STYLE") e.disabled = true;
-    delete e.style;
+    e.style = "";
   });
   [].forEach.call(document.links, function(a) {
-    var img = a.firstChild;
+    var img = a.querySelector("img");
     img.src = a.href;
     a.style = "float:left;margin:0 100% 1em 0;";
   });
