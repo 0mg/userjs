bf159c6b66356b52ef3d7c7ca180d49450d5e765
0mg <millig@outlook.jp>
2018-09-14 15:24:12 +0900

    upd: renew keyjack.js(Ctrl+8 to toggle)

--- keyjack.js
+++ keyjack.js
@@ -5,16 +5,14 @@
 (function() {
   var keyJack = (function() {
     var pressCount = 0;
+    var disableKey = false;
     return function callee(v) {
       if (v.target instanceof HTMLInputElement ||
           v.target instanceof HTMLTextAreaElement) return;
-      v.stopImmediatePropagation();
-      if (v.type === "keyup" && v.keyCode === 75 && v.ctrlKey) {
+      if (disableKey) v.stopImmediatePropagation();
+      if (v.type === "keyup" && v.key === "8" && v.ctrlKey) {
         if (pressCount >= 1) {
-          removeEventListener("keypress", callee, true);
-          removeEventListener("keydown", callee, true);
-          removeEventListener("keyup", callee, true);
-          alert("Enabled page's keyboard shortcuts");
+          disableKey = !disableKey;
         } else {
           ++pressCount;
           setTimeout(function() { pressCount = 0; }, 180);
