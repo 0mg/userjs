4ffc749ca9b6b1824f128e8a1a3f1d0bd6e8024e
0mg <millig@outlook.jp>
2018-09-16 17:43:47 +0900

    upd: added function notify() > htmledit, keyjack

--- htmledit.js
+++ htmledit.js
@@ -1,8 +1,28 @@
 addEventListener("keyup", function(v) {
+  var notify = function(str) {
+    var box = document.createElement("div");
+    box.textContent = str;
+    box.style.background = "rgba(0, 0, 0, 0.8)";
+    box.style.color = "white";
+    box.style.fontSize = "xx-large";
+    box.style.display = "inline-block";
+    box.style.position = "fixed";
+    box.style.transform = "translate(-50%, -50%)";
+    box.style.top = "50%";
+    box.style.left = "50%";
+    box.style.padding = "1ex";
+    box.style.borderRadius = "1ex";
+    box.style.zIndex = "2147483647";
+    document.body.appendChild(box);
+    setTimeout(function() {document.body.removeChild(box);}, 700);
+  };
   if (v.key === "9" && v.ctrlKey) {
     document.designMode = document.designMode !== "on" ? "on" : "off";
   }
   else if (v.key === "Escape") {
+    if (document.designMode !== "on") return;
     document.designMode = "off";
   }
+  else return;
+  notify("📝: " + document.designMode);
 });
--- keyjack.js
+++ keyjack.js
@@ -3,6 +3,23 @@
 // ==/UserScript==
 
 (function() {
+  var notify = function(str) {
+    var box = document.createElement("div");
+    box.textContent = str;
+    box.style.background = "rgba(0, 0, 0, 0.8)";
+    box.style.color = "white";
+    box.style.fontSize = "xx-large";
+    box.style.display = "inline-block";
+    box.style.position = "fixed";
+    box.style.transform = "translate(-50%, -50%)";
+    box.style.top = "50%";
+    box.style.left = "50%";
+    box.style.padding = "1ex";
+    box.style.borderRadius = "1ex";
+    box.style.zIndex = "2147483647";
+    document.body.appendChild(box);
+    setTimeout(function() {document.body.removeChild(box);}, 700);
+  };
   var keyJack = (function() {
     var pressCount = 0;
     var disableKey = false;
@@ -11,8 +28,9 @@
           v.target instanceof HTMLTextAreaElement) return;
       if (disableKey) v.stopImmediatePropagation();
       if (v.type === "keyup" && v.key === "8" && v.ctrlKey) {
-        if (pressCount >= 1) {
+        if (pressCount >= 0) {
           disableKey = !disableKey;
+          notify("⌨: " + (disableKey ? "OFF" : "ON"));
         } else {
           ++pressCount;
           setTimeout(function() { pressCount = 0; }, 180);
