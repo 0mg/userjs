b4347b15e8cfb198f9e9921b33cb3c0903795c7c
0mg <millig@outlook.jp>
2018-10-18 18:00:05 +0900

    fix ytchat.js: functionable everytime

--- ytchat.js
+++ ytchat.js
@@ -24,7 +24,7 @@ if (location.href.startsWith("https://www.youtube.com/live_chat")) (function() {
   });
 })();
 
-if (location.href.startsWith("https://www.youtube.com/watch")) (function() {
+else (function() {
   var on = false;
   var notify = function(str) {
     var box = document.createElement("dialog");
@@ -35,29 +35,39 @@ if (location.href.startsWith("https://www.youtube.com/watch")) (function() {
     box.showModal();
     setTimeout(function() {document.body.removeChild(box);}, 700);
   };
-  var container = document.createElement("pre");
-  container.style.position = "relative";
-  container.style.opacity = "0.7";
-  container.style.zIndex = "50";
-  container.style.top = "0";
-  container.style.left = "0";
-  container.style.color = "white";
-  container.style.fontSize = "x-large";
-  container.style.textShadow = "1px 1px 1px black";
-  addEventListener("load", function() {
-    document.querySelector(".html5-video-player").appendChild(container);
-  });
+  var container_id = "chatbox_on_video";
+  function addChatbox() {
+    var video = document.querySelector(".html5-video-player");
+    if (!video) return false;
+    else if (document.getElementById(container_id)) return true;
+    var container = document.createElement("pre");
+    container.id = container_id;
+    container.style.position = "relative";
+    container.style.opacity = "0.7";
+    container.style.zIndex = "50";
+    container.style.top = "0";
+    container.style.left = "0";
+    container.style.color = "white";
+    container.style.fontSize = "x-large";
+    container.style.textShadow = "1px 1px 1px black";
+    video.appendChild(container);
+    return true;
+  }
+  addEventListener("load", addChatbox);
+  var timer = setInterval(addChatbox, 500);
   addEventListener("keyup", function(event) {
+    var container = document.getElementById(container_id);
     if (event.key === "7" && event.ctrlKey) {
       on = !on;
       notify("💭 " + on);
       if (!on) {
-        container.textContent = "";
+        if (container) container.textContent = "";
       }
     }
   });
   addEventListener("message", function(event) {
-    if (!on) return;
+    var container = document.getElementById(container_id);
+    if (!on || !container) return;
     if (event.origin === "https://www.youtube.com") {
       var json = JSON.parse(event.data);
       if (json.type === "chat-text") {
