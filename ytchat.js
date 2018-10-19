43cd6eef2fd8fb427a78b6eff0d0ebd659b69911
0mg <millig@outlook.jp>
2018-10-19 19:22:38 +0900

    upd ytchat: added button. disable ctrl+7.

--- ytchat.js
+++ ytchat.js
@@ -5,76 +5,36 @@
 
 if (location.href.startsWith("https://www.youtube.com/live_chat")) (function() {
   if (parent === self) return;
-  var obs = new MutationObserver(function(records) {
-    [].forEach.call(records, function(item) {
-      [].forEach.call(item.addedNodes, function(elem) {
-        if (elem.localName !== "yt-live-chat-text-message-renderer") return;
-        var body = elem.querySelector("#message");
+  // Program: Chat data getter
+  var mo = new MutationObserver(function(records) {
+    for (var rcd of records) {
+      for (var nd of rcd.addedNodes) {
+        var body = nd.querySelector("#message");
+        if (!body) continue;
         var rawtext = "";
-        for (var i = 0; i < body.childNodes.length; i++) {
-          var current = body.childNodes[i];
-          if (current.nodeName === "#text") rawtext += current.nodeValue;
-          else if (current.nodeName === "IMG") rawtext += current.alt;
+        for (var phrase of body.childNodes) {
+          rawtext += phrase.alt || phrase.textContent;
         }
         var msg = {
           type: "chat-text",
           data: rawtext
         };
         parent.postMessage(JSON.stringify(msg), "https://www.youtube.com");
-      });
-    });
+      }
+    }
   });
-  addEventListener("load", function() {
-    obs.observe(document.querySelector("#contents.yt-live-chat-renderer"),
-      {childList:true, subtree:true});
+  addEventListener("DOMContentLoaded", function() {
+    mo.observe(document.querySelector("#items.yt-live-chat-item-list-renderer"),
+      {childList:true});
   });
 })();
 
 else (function() {
-  var on = false;
-  var notify = function(str) {
-    var box = document.createElement("dialog");
-    box.textContent = str;
-    box.style.fontSize = "xx-large";
-    box.style.borderRadius = "1ex";
-    document.body.appendChild(box);
-    box.showModal();
-    setTimeout(function() {document.body.removeChild(box);}, 700);
-  };
-  var container_id = "chatbox_on_video";
-  function addChatbox() {
-    var video = document.querySelector(".html5-video-player");
-    if (!video) return false;
-    else if (document.getElementById(container_id)) return true;
-    var container = document.createElement("pre");
-    container.id = container_id;
-    container.style.position = "relative";
-    container.style.opacity = "0.7";
-    container.style.zIndex = "50";
-    container.style.top = "0";
-    container.style.left = "0";
-    container.style.color = "white";
-    container.style.fontSize = "x-large";
-    container.style.textShadow = "1px 1px 1px black";
-    container.style.width = "0";
-    video.appendChild(container);
-    return true;
-  }
-  addEventListener("load", addChatbox);
-  var timer = setInterval(addChatbox, 500);
-  addEventListener("keyup", function(event) {
-    var container = document.getElementById(container_id);
-    if (event.key === "7" && event.ctrlKey) {
-      on = !on;
-      notify("💭 " + on);
-      if (!on) {
-        if (container) container.textContent = "";
-      }
-    }
-  });
+  // Program: Chat render/updater
+  var doing = false;
   addEventListener("message", function(event) {
     var container = document.getElementById(container_id);
-    if (!on || !container) return;
+    if (!doing || !container) return;
     if (event.origin === "https://www.youtube.com") {
       var json = JSON.parse(event.data);
       if (json.type === "chat-text") {
@@ -87,4 +47,51 @@ else (function() {
       }
     }
   });
+  // UI: Chat box on video
+  const container_id = "chatbox_on_video";
+  var addChatbox = function() {
+    var video = document.querySelector(".html5-video-player");
+    if (!video) return false;
+    else if (document.getElementById(container_id)) return true;
+    var container = document.createElement("pre");
+    container.id = container_id;
+    container.style.position = "relative";
+    container.style.zIndex = "50";
+    container.style.width = "0";
+    container.style.opacity = "0.7";
+    container.style.fontSize = "x-large";
+    container.style.textShadow = "1px 1px 1px black";
+    container.style.color = "white";
+    video.appendChild(container);
+    return true;
+  };
+  setInterval(addChatbox, 1000);
+  // UI: Controller (on,off)
+  const ctrlbtn_id = "chat_on_video_switch";
+  var switchUI = function(doing) {
+    var container = document.getElementById(container_id);
+    if (container) container.hidden = !doing;
+    var btn = document.getElementById(ctrlbtn_id);
+    if (btn) btn.style.opacity = 0.5 + doing;
+  };
+  var addCtrlBtn = function() {
+    var dst = document.querySelector(".ytp-right-controls");
+    if (!dst) return false;
+    else if (document.getElementById(ctrlbtn_id)) return true;
+    var imitee = dst.querySelector("button");
+    if (!imitee) return false;
+    var btn = imitee.cloneNode(true);
+    btn.addEventListener("click", function() {
+      doing = !doing;
+      switchUI(doing);
+    });
+    btn.id = ctrlbtn_id;
+    btn.style = "";
+    btn.title = "chat overlay";
+    btn.firstChild.innerHTML = `<text y="24" x="7" font-size="18">💭</text>`;
+    dst.insertBefore(btn, dst.firstChild);
+    switchUI(doing);
+    return true;
+  };
+  setInterval(addCtrlBtn, 1000);
 })();
