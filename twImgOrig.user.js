56aeed2730efa5a6e7e57fffc7c735ea9a5680e7
0mg <millig@outlook.jp>
2018-10-19 19:21:47 +0900

    dev twimgorig: golf

--- twImgOrig.user.js
+++ twImgOrig.user.js
@@ -1,18 +1,12 @@
 // ==UserScript==
 // @name OpenTweetImageOrig
-// @description ツイート画像をクリックで原寸大表示します. (Click to open tweet image as original size)
-// @version 1.0
 // @include https://twitter.com/*
-// @namespace https://greasyfork.org/users/8032
 // ==/UserScript==
 
-window.addEventListener("click", function(event) {
+addEventListener("click", function(event) {
   var elem = event.target;
-  if (elem.src.indexOf("https://pbs.twimg.com/media/") === 0) {
-    var imgURL = elem.src;
-    var imgURLorig = imgURL.replace(/:[^/]+$|$/, ":orig");
-    window.open(imgURLorig, "_blank");
-    //event.stopImmediatePropagation();
+  if (elem.src && elem.src.startsWith("https://pbs.twimg.com/media/")) {
+    window.open(elem.src.replace(/:[^/]+$|$/, ":orig"), "_blank");
     event.preventDefault();
   }
 }, true);
